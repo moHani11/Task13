5d0a24c7d1382a90802d65468db99a285056cd67
mohamed hani ebrahim <mohamedhani004@gmail.com>
2024-09-16 15:08:38 +0300

    First commit for yolo task

---
+++ GameEngine.py
@@ -0,0 +1,205 @@
+import cv2
+import numpy as np
+from ultralytics import YOLO
+from time import sleep
+
+model = YOLO("yolov8n.pt")
+
+
+X_OFFSET = 20
+Y_OFFSET = 20
+
+xFound = False
+oFound = False
+x_cords = (0,0)
+o_cords = (0,0)
+
+Xs = []
+Os = []
+turn = 1 # X turn in the beginning
+winner = -1 # -1 refers to no winner
+
+grid = [[-1, -1, -1],
+        [-1, -1, -1],
+        [-1, -1, -1]]
+# A simulated grid to check the conditions of the game
+
+winner_disp_counter = 0
+
+def checkGameOver(): ## checks for game over using the simulated grid
+
+    for row in grid:
+        if row[0] != -1 and row[0] == row[1] == row[2]:
+            return row[0]  
+    for col in range(3):
+        if grid[0][col] != -1 and grid[0][col] == grid[1][col] == grid[2][col]:
+            return grid[0][col]
+    
+    if grid[0][0] != -1 and grid[0][0] == grid[1][1] == grid[2][2]:
+        return grid[0][0]
+    
+    if grid[0][2] != -1 and grid[0][2] == grid[1][1] == grid[2][0]:
+        return grid[0][2]
+
+    if (all(cell != -1 for row in grid for cell in row) ):
+        return 2
+
+    
+    return -1
+
+def checkPos(cords):  # gets the position in the simulated grid with respect to its actual position in the frame
+    for i in range (3):
+        for j in range(3):
+            if (cords[0] >= j*x_lines and cords[0] <= (j+1)*x_lines) and (cords[1] >= i*y_lines and cords[1] <= (i+1)*y_lines):
+                return (i,j)
+    return (-500,-500)
+
+
+def DrawX(posX, posY):  # Draws an X in the given position
+    cv2.line(frame, (x_lines*posY+X_OFFSET, y_lines*posX+Y_OFFSET), (x_lines*(posY+1)-X_OFFSET, y_lines*(posX+1)-Y_OFFSET ), (10,80,240), 7)
+    cv2.line(frame, (x_lines*posY+X_OFFSET, y_lines*(posX+1)-Y_OFFSET), (x_lines*(posY+1)-X_OFFSET, y_lines*posX+Y_OFFSET ), (10,80,240), 7)
+    grid[posX][posY] = 1
+
+def DrawO(posY, posX): # draws O in the given position
+    x_center = (posX*x_lines + (posX+1)*x_lines)//2
+    y_center = (posY*y_lines + (posY+1)*y_lines)//2
+    r = min([x_lines, y_lines])//2 - 10
+    cv2.circle(frame, (x_center, y_center), r, (240,80,10), 7)
+    grid[posY][posX] = 0
+
+
+
+##////////////////////////////////////////////////////////////////////////
+# Main loop
+
+cap = cv2.VideoCapture(0)
+if not cap.isOpened():
+    print("Error")
+    exit()
+
+while True:
+    ret, frame = cap.read()
+    frame = cv2.resize(frame, (900, 500))
+    if not ret:
+        print("Error no frame")
+        break
+    X_FRAME, Y_FRAME = frame.shape[1], frame.shape[0]
+    
+    x_lines = X_FRAME // 3
+    y_lines = Y_FRAME // 3
+
+#############################################
+
+    if winner == -1: 
+
+        results = model.predict(frame, conf = 0.4,  vid_stride = 20, verbose=False, max_det = 10)
+
+        for result in results:
+                boxes = result.boxes
+
+                for box in boxes:
+                    conf = box.conf.cpu().numpy()
+                    cls = box.cls.cpu().numpy().astype(int)
+                    if cls == 77 and turn == 1: # cls will be changed when we train the model and get the right class number
+                        label = model.names[cls[0]]
+                        print(cls)
+                        x1, y1, x2, y2 = box.xyxy[0].cpu().numpy().astype(int)
+                        
+                        cx = (x1 + x2) // 2
+                        cy = (y1 + y2) // 2
+                        
+                        # cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                        cv2.circle(frame, (cx, cy), 15, (0, 0, 255), -1)
+                        xFound = True # declairing we detected am x gesture 
+                        x_cords = (cx, cy) # setting the X coordinates
+                        
+
+                    elif cls == 77 and turn == 0:  # cls will be changed when we train the model and get the right class number
+                        label = model.names[cls[0]]
+                        print(cls)
+                        x1, y1, x2, y2 = box.xyxy[0].cpu().numpy().astype(int)
+                        
+                        cx = (x1 + x2) // 2
+                        cy = (y1 + y2) // 2
+                        
+                        cv2.circle(frame, (cx, cy), 15, (0, 0, 255), -1)
+                        oFound = True # declairing we detected am O gesture 
+                        o_cords = (cx, cy) # setting the O coordinates
+                            
+
+
+    ######################################################
+        
+        for x in range(x_lines, X_FRAME-3, x_lines ):
+            cv2.line(frame, (x,0+Y_OFFSET), (x,Y_FRAME-Y_OFFSET), (100,150,190), 5)
+
+        for y in range(y_lines, Y_FRAME-3, y_lines ):
+            cv2.line(frame, (0+X_OFFSET, y), (X_FRAME-X_OFFSET, y), (100,150,190), 5)
+
+
+        if xFound: # in case an X gesture was detected we check the cell if its empty and if it is
+            (row, col) = checkPos(x_cords) # we draw an X in it and switch turns then we checks for game over 
+            if row < 0: break 
+            if grid[row][col] == -1:
+                grid[row][col] = 1
+                Xs.append((row, col))
+                turn = 0
+                winner = checkGameOver()
+                sleep(0.005)
+
+            xFound = False
+        
+
+        if oFound:  # in case an O gesture was detected
+            (row, col) = checkPos(o_cords)
+            if row < 0: break 
+            if grid[row][col] == -1:
+                grid[row][col] = 0
+                Os.append((row, col))
+                turn = 1
+                winner = checkGameOver()
+                sleep(0.005)
+
+
+            oFound = False
+        
+
+        for x in Xs:
+            DrawX(x[0], x[1])  # We draw all the X symbols in our grid 
+        for o in Os:
+            DrawO(o[0], o[1])  # We draw all the O symbols in our grid 
+
+
+    else: # in case a winner was declaired 
+        winner_disp_counter+=1
+        if winner == 1: winner_text = f"X Won The Game!"
+        if winner == 0: winner_text = f"O Won The Game!"
+        if winner == 2: winner_text = f"It's a Draw"
+        cv2.putText(frame, winner_text, (X_FRAME//10, Y_FRAME//2), cv2.FONT_HERSHEY_SIMPLEX, 2, (150, 170, 90), 5)
+        cv2.putText(frame, f"The game will repeat in {(300 - winner_disp_counter)//40}", (X_FRAME//8, Y_FRAME-40), cv2.FONT_HERSHEY_SIMPLEX, 1, (150, 170, 90), 3)
+        cv2.putText(frame, f"Press 'q' to quit", (X_FRAME//8, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (150, 170, 90), 3)
+
+        if winner_disp_counter >= 300:
+            winner = -1
+            grid = [[-1, -1, -1],
+                    [-1, -1, -1],
+                    [-1, -1, -1]]
+            
+            Xs = []
+            Os = []
+            turn = 1
+            winner_disp_counter = 0
+            xFound = False
+            oFound = False
+
+            
+            
+
+
+
+    cv2.imshow('Video', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
--- README.md
+++ README.md
@@ -1,4 +1,49 @@
-## Task 13 Repo
+## This branch shows our work in task 13.1 which should work as follows:
 
-- This repo shows our work(Group 3) in Task13.1 and Task13.2
-- Each task has its own branch with its own description in its README file
+- Develop a real-time Tic-Tac-Toe game using computer vision, powered by an Object Detection model (YOLO). The game should recognize hand gestures and convert them into moves on the grid.
+
+## Repo's content
+
+- A description for our code in this README file
+- Python code used as the game engine
+- **[Drive link](https://drive.google.com/drive/folders/1c8QZ-NIJ0zkRA0k8OvTMj14AMF7L-dT7)** for the dataset we made and used to train the model
+
+----
+----
+
+
+# Summary Documentation of the Code
+
+> This Python script serves as the game engine for a tic tac toe game that uses gesture detection to play. The engine is designed to detect X and O gestures in real-time using OpenCV and YOLOv8. While the engine itself is complete, the missing piece is training the YOLO model to detect the specific X and O gestures. Once the model is trained, it can be used to control the gameplay by detecting player moves. For trying the engine I have used a small toy refering to class 77 and it works perfectly well.
+
+## Key Components in the code:
+
+### YOLOv8 Object Detection:
+
+- The current class i was using (cls == 77) need to be replaced with the correct class numbers after the model is trained on custom data for detecting X and O gestures.
+Bounding boxes are used to identify the location of the gesture within the frame, and the center of the detected gesture is mapped to a position on the tic-tac-toe grid.
+
+### Tic-Tac-Toe Game Logic:
+
+>The game is played on a 3x3 grid (grid) where:
+>1 represents X,
+>0 represents O,
+>-1 represents an empty cell.
+>After each gesture is detected and mapped to the grid, the engine checks for a winner or a draw using the checkGameOver() function.
+
+#### Turn Management:
+
+>The game alternates between players:
+>X’s turn is represented by turn = 1,
+>O’s turn is represented by turn = 0.
+>Each player's gesture is detected and only applied if it’s their turn.
+
+#### Drawing the Board:
+
+X symbols are drawn using DrawX().
+O symbols are drawn using DrawO().
+Gridlines are dynamically drawn to divide the frame into 3x3 sections representing the game board.
+
+#### Declaring a Winner:
+
+- When a player wins, the game displays a message (X Won, O Won, or Draw) on the screen. After a brief countdown the game resets for a new round.
